68b03abe4698b3e40d8cea9fa94789a575fd0e7d
Brain4Tech <Jefta.deLeeuw@web.de>
2022-04-10 16:12:48 +0200

    correct login

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "da4971aed21dbd11ca8a577940776480",
 "manifest.json": "0bb3eae56498ba29f53542f9019688b9",
-"main.dart.js": "7b509a29d6bfc5210ae6a07991dc63fb",
+"main.dart.js": "11a71d7d34f070146e2067b3d9a84960",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -19,7 +19,7 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "76e7ad01279292eb837b2c175a61d2b1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "6c262318c0d4871b885fe747a9dd8204",
+"assets/NOTICES": "7e321321fe7755c3784e6b3710006c7e",
 "assets/assets/images/flags/de.png": "142e3a1ed32478f0f086c139de0015ed",
 "assets/assets/images/flags/ua.png": "56738557edf06f73c9e3d45f875bd204",
 "assets/assets/images/flags/nl.png": "a35c0a7edb829c4e2c844b3302266c9a",
@@ -29,8 +29,8 @@ const RESOURCES = {
 "assets/assets/images/default_profile_picture.svg": "5a0e4db6a31eed85b7dce8b5c31352fa",
 "assets/assets/images/default_profile_picture.png": "5f525e6a52c3a0b322f4b4e7e7b94bea",
 "assets/assets/images/default_profile_picture_small.png": "dd10a666d1638d76c918ee006313075b",
-"index.html": "fe0a787ea798557b560174eecb93ec41",
-"/": "fe0a787ea798557b560174eecb93ec41"
+"index.html": "b4d494f2d722db298265850df0e5b990",
+"/": "b4d494f2d722db298265850df0e5b990"
 };
 
 // The application shell files that are downloaded before a service worker can
